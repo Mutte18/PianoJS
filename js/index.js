b1b8453a927f1213a433ce8d1d742c578bc882b4
mattias <mattias.vidhall@hotmail.com>
2019-02-02 23:56:22 +0100

    Got the piano-layout working. Next is to create the questionprompt

--- js/index.js
+++ js/index.js
@@ -1,55 +1,59 @@
-//document.querySelector('h1').style.color = "red";
-const pianoDiv = document.querySelector('.pianoContainer');
-for (let i = 0; i < 22; i++) {
-    let letter = "";
-
-    if(i % 7 === 0){
-        letter = "C";
-
+function createPiano(octaves){
+    const pianoHTML = document.querySelector('.pianoContainer');
+    for(let i = 0; i < octaves; i++){
+        pianoHTML.insertAdjacentHTML('beforeend', getPianoTangents(i));
     }
-    else if(i % 7 === 1){
-        letter = "D";
-
-    }
-    else if(i % 7 === 2){
-        letter = "E";
-
-    }
-    else if(i % 7 === 3){
-        letter = "F";
-
-    }
-    else if(i % 7 === 4){
-        letter = "G";
-
-    }
-    else if(i % 7 === 5){
-        letter = "A";
-
-    }
-    else if(i % 7 === 6){
-        letter = "B";
-
-    }
-
-
-
-
-
-    const pianoElement =
-        `<div class="pianoTangent" id="${i}">
-                ${letter}
-            </div>`;
-    pianoDiv.insertAdjacentHTML('beforeend', pianoElement);
 }
-;
+
+function getPianoTangents(id){
+    const html =
+        `<div class="key white c" id="c${id}">
+                C
+            </div>
+            <div class="key black cs" id="cs${id}">
+                cs
+            </div>
+            <div class="key white d" id="d${id}">
+                D
+            </div>
+            <div class="key black ds" id="ds${id}">
+                ds
+            </div>
+            <div class="key white e" id="e${id}">
+                E
+            </div>
+            <div class="key white f" id="f${id}">
+                F
+            </div>
+            <div class="key black fs" id="fs${id}">
+                fs
+            </div>
+            <div class="key white g" id="g${id}">
+                G
+            </div>
+            <div class="key black gs" id="gs${id}">
+                gs
+            </div>
+            <div class="key white a" id="a${id}">
+                A
+            </div>
+            <div class="key black as" id="as${id}">
+                as
+            </div>
+            <div class="key white b" id="b${id}">
+                B
+            </div>`;
+    return html;
+}
+
+function chooseTangent(id) {
+    console.log(document.getElementById(id).id);
+}
 
 const pianoTanget = document.querySelector('.pianoContainer');
 pianoTanget.addEventListener('click', e => {
     chooseTangent(e.target.id);
 });
 
-function chooseTangent(id) {
-    console.log(document.getElementById(id));
-
-}
+createPiano(3);
+//initializePiano();
